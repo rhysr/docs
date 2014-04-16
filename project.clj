4d0dbd220b8d76e41283fed73e161143c434865d
rhysr <rhysr@mega-city-one.co.uk>
2014-04-16 20:12:37 +0100

    fix typo

--- project.clj
+++ project.clj
@@ -13,4 +13,4 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencis [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}})
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}})

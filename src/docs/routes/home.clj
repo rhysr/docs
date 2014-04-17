8142364fff8fc6ad3f8de7971fad205e61b4c209
rhysr <rhysr@mega-city-one.co.uk>
2014-04-17 19:16:19 +0100

    generate html with hiccup

--- src/docs/routes/home.clj
+++ src/docs/routes/home.clj
@@ -1,10 +1,14 @@
 (ns docs.routes.home
-  (:require [compojure.core :refer [defroutes GET]]))
+  (:require [compojure.core :refer [defroutes GET]]
+            [hiccup.page :refer [html5]]))
 
 
 (defn home []
   {:status 200
-   :body "<!DOCTYPE html>\n<html><head></head><body><h1>Docs</h1></body></html>"})
+   :body (html5
+           [:head]
+           [:body
+            [:h1 "Docs"]])})
 
 
 (defroutes home-routes

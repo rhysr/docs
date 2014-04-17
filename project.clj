be719acc80fa6060bb9fcb38ceff6018afe5d757
rhysr <rhysr@mega-city-one.co.uk>
2014-04-17 19:10:34 +0100

    don't open browser in dev

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Document Storage App"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.6"]
-                 [ring-server "0.3.1"]]
+                 [ring-server "0.3.1"]
+                 [hiccup "1.0.5"]]
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler docs.handler/app
          :init docs.handler/init
@@ -13,4 +14,5 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}})
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]
+    :ring {:open-browser? false}}})
